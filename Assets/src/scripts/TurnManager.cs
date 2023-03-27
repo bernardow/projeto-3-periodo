5da15b2b934a2b1c1af8dd5958e127f7b1bbe73f
bernardow <bernardoritzel@gmail.com>
2023-03-27 09:29:12 -0300

    turn-management-ready

--- Assets/src/scripts/TurnManager.cs
+++ Assets/src/scripts/TurnManager.cs
@@ -6,18 +6,39 @@ using UnityEngine.Rendering;
 
 public class TurnManager : MonoBehaviour
 {
-    private Stack turn = new Stack();
-    private string p1Turn = "Vez 1";
-    private string p2Turn = "Vez 2";
+    public List<string> turn = new List<string>();
+    private string p1Turn = "Player 1";
+    private string p2Turn = "Player 2";
+
+    [SerializeField] private GameObject p1 = null;
+    [SerializeField] private GameObject p2 = null;
 
     private void Start()
     {
-        turn.Push(p2Turn);
-        turn.Push(p1Turn);
+        turn.Add(p2Turn);
+        turn.Add(p1Turn);
     }
 
-    public void CheckTurn()
+    public void SkipTurn()
     {
-        Debug.Log(turn.Peek());
+        turn.Reverse();
+        TurnManagement();
     }
+
+    private void TurnManagement()
+    {
+        if (FirstInQueue(turn) == p1Turn)
+        {
+            p1.SetActive(true);
+            p2.SetActive(false);
+        }
+        else
+        {
+            p1.SetActive(false);
+            p2.SetActive(true);
+        }
+    }
+    
+    private string FirstInQueue(List<string> list) => list[list.Count - 1];
+    
 }
