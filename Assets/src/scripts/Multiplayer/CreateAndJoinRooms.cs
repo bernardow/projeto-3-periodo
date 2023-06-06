7b0c810b2bda43a3c313f85443c73a6aabb545f7
Bernardo Ritzel <bernardoritzel@gmail.com>
2023-06-06 09:08:17 -0300

    new-lobby

--- Assets/src/scripts/Multiplayer/CreateAndJoinRooms.cs
+++ Assets/src/scripts/Multiplayer/CreateAndJoinRooms.cs
@@ -8,6 +8,12 @@ using TMPro;
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
     public TMP_InputField roomNameInput;
+    [SerializeField] private TMP_InputField nicknameField;
+    [SerializeField] private Button startGameBtn;
+    [SerializeField] private List<TextMeshProUGUI> nicknameTexts = new List<TextMeshProUGUI>();
+
+    [SerializeField] private GameObject lobbyUI;
+    [SerializeField] private GameObject roomUI;
     
     public void CreateRoom()
     {
@@ -21,6 +27,48 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
+        ChangeUI(roomUI);
+        photonView.RPC("UpdatePlayerList", RpcTarget.AllBuffered);
+        startGameBtn.interactable = PhotonNetwork.IsMasterClient;
+    }
+
+    [PunRPC]
+    private void UpdatePlayerList()
+    {
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            Debug.Log(PhotonNetwork.PlayerList[i].NickName);
+            nicknameTexts[i].text = PhotonNetwork.PlayerList[i].NickName;
+        }
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        UpdatePlayerList();
+    }
+
+    public void StartGame()
+    {
+        photonView.RPC("LoadLevel", RpcTarget.AllBuffered, "TestScene");
+    }
+
+    [PunRPC]
+    public void LoadLevel(string levelName)
+    {
         PhotonNetwork.LoadLevel("TestScene");
+    }
+
+    private void ChangeUI(GameObject currentUI)
+    {
+        roomUI.SetActive(false);
+        lobbyUI.SetActive(false);
+        
+        currentUI.SetActive(true);
+    }
+
+    public void SetNickName()
+    {
+        PhotonNetwork.NickName = nicknameField.text;
     }
 }
