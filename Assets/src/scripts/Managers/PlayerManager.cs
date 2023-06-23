f81e82c6820cc06ff17f8a53bbe6d342129a4b3c
Bernardo Ritzel <bernardoritzel@gmail.com>
2023-06-23 11:02:08 -0300

    buttn-disable-able

--- Assets/src/scripts/Managers/PlayerManager.cs
+++ Assets/src/scripts/Managers/PlayerManager.cs
@@ -1,24 +1,38 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 namespace src.scripts.Managers
 {
     public class PlayerManager : MonoBehaviour
     {
+        private Button _skipTurnBtn;
         public int cardsPulled;
         public bool canPull;
         public int playerCardsNum;
         public int mergedCards;
         public int droppedCards;
 
+        private void Awake() => _skipTurnBtn = GameObject.Find("Turn Btn").GetComponent<Button>();
+
         private void OnEnable()
         {
             cardsPulled = 0;
             mergedCards = 0;
             droppedCards = 0;
+            CanSkipTurn(true);
         }
 
-        private void Update() => CanPull();
+        private void OnDisable() => CanSkipTurn(false);
+
+        private void Update()
+        {
+            CanPull();
+        }
+
+        private void CanSkipTurn(bool state) => _skipTurnBtn.interactable = state;
+        
         
         private void CanPull()
         {
